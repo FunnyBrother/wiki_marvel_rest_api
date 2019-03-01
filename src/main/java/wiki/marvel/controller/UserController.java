f11189f01e0854a8f61ab3321832e1e6bbefc6e4
funnybrother <funbro3110>
2019-03-01 18:09:10 +0200

    --- modify UserController: @Autowired fields UserService and ModelMapper

--- src/main/java/wiki/marvel/controller/UserController.java
+++ src/main/java/wiki/marvel/controller/UserController.java
@@ -1,8 +1,20 @@
 package wiki.marvel.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import wiki.marvel.service.UserService;
 
 @RestController(value = "/user")
 public class UserController {
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
 
 }
