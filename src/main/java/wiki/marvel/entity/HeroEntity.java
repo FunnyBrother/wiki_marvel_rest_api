adb0cef07e407837825213584210fd52e9f1530e
funnybrother <funbro3110>
2019-03-01 17:22:40 +0200

    --- correct fields in HeroEntity

--- src/main/java/wiki/marvel/entity/HeroEntity.java
+++ src/main/java/wiki/marvel/entity/HeroEntity.java
@@ -3,6 +3,8 @@ package wiki.marvel.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 @Data
 @Entity
@@ -13,11 +15,16 @@ public class HeroEntity {
     private Long id;
 
     @Column(nullable = false)
+    @Min(value = 8)
+    @Max(value = 32)
     private String name;
 
-    @Column(name = "fullname", nullable = false)
-    private String fullName;
+    @Column(nullable = false)
+    @Min(value = 8)
+    @Max(value = 32)
+    private String fullname;
 
     @Column(nullable = false)
+    @Max(value = 255)
     private String description;
 }
