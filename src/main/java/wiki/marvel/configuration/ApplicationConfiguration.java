fb09a254e535b59a05061c0b9256c9da6e70bd25
funnybrother <funbro3110>
2019-03-01 18:06:40 +0200

    --- modify ApplicationConfiguration: add bean 'getUserService'

--- src/main/java/wiki/marvel/configuration/ApplicationConfiguration.java
+++ src/main/java/wiki/marvel/configuration/ApplicationConfiguration.java
@@ -4,14 +4,22 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import wiki.marvel.repository.HeroRepository;
+import wiki.marvel.repository.UserRepository;
 import wiki.marvel.service.HeroService;
+import wiki.marvel.service.UserService;
 import wiki.marvel.service.implementation.HeroServiceImpl;
+import wiki.marvel.service.implementation.UserServiceImpl;
 
 @Configuration
 public class ApplicationConfiguration {
     @Bean
     public HeroService getHeroService(HeroRepository heroRepository) {
         return new HeroServiceImpl(heroRepository);
+    }
+
+    @Bean
+    public UserService getUserService(UserRepository userRepository) {
+        return new UserServiceImpl(userRepository);
     }
 
     @Bean
