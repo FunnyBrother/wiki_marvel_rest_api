5c924f31907af001996d4da31653ca5c5bada2a5
funnybrother <funbro3110>
2019-03-01 18:03:13 +0200

    --- correct bean 'getHeroService' into ApplicationConfiguration class

--- src/main/java/wiki/marvel/configuration/ApplicationConfiguration.java
+++ src/main/java/wiki/marvel/configuration/ApplicationConfiguration.java
@@ -3,14 +3,15 @@ package wiki.marvel.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import wiki.marvel.repository.HeroRepository;
 import wiki.marvel.service.HeroService;
 import wiki.marvel.service.implementation.HeroServiceImpl;
 
 @Configuration
 public class ApplicationConfiguration {
     @Bean
-    public HeroService getHeroService() {
-        return new HeroServiceImpl();
+    public HeroService getHeroService(HeroRepository heroRepository) {
+        return new HeroServiceImpl(heroRepository);
     }
 
     @Bean
