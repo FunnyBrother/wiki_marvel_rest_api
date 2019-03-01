f04e9a30a6bd5c9d2a72f186b944f4908c64f5ec
funnybrother <funbro3110>
2019-03-01 17:40:41 +0200

    --- new field is HeroDto: UserEntity

--- src/main/java/wiki/marvel/dto/HeroDto.java
+++ src/main/java/wiki/marvel/dto/HeroDto.java
@@ -1,11 +1,13 @@
 package wiki.marvel.dto;
 
 import lombok.Data;
+import wiki.marvel.entity.UserEntity;
 
 @Data
 public class HeroDto {
     private Long id;
     private String name;
-    private String fullName;
+    private String nickname;
     private String description;
+    private UserEntity user;
 }
