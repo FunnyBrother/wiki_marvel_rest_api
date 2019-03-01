80200d79e8bfdbf450fb0807d8a6738d491e33d3
funnybrother <funbro3110>
2019-03-01 18:00:41 +0200

    --- create UserService class

---
+++ src/main/java/wiki/marvel/service/UserService.java
@@ -0,0 +1,22 @@
+package wiki.marvel.service;
+
+import org.springframework.stereotype.Service;
+import wiki.marvel.entity.HeroEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface UserService {
+    HeroEntity save(HeroEntity entity);
+
+    Optional<HeroEntity> findById(Long id);
+
+    boolean existsById(long id);
+
+    List<HeroEntity> findAll();
+
+    void deleteById(Long id);
+
+    void delete(HeroEntity entity);
+}
