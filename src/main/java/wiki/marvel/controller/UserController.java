8ac552ca64a1c53d810a039e1e8db2a1f69f333c
funnybrother <funbro3110>
2019-03-04 12:51:27 +0200

    --- add method UserController#delete to delete user by user entity(description)

--- src/main/java/wiki/marvel/controller/UserController.java
+++ src/main/java/wiki/marvel/controller/UserController.java
@@ -2,8 +2,18 @@ package wiki.marvel.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import wiki.marvel.dto.UserDto;
+import wiki.marvel.entity.UserEntity;
 import wiki.marvel.service.UserService;
+
+import java.util.Map;
+
+import static wiki.marvel.enums.UserControllerMessagesEnum.NOT_FOUND_USER_BY_ID;
 
 @RestController(value = "/user")
 public class UserController {
@@ -16,5 +26,20 @@ public class UserController {
         this.modelMapper = modelMapper;
     }
 
+    @DeleteMapping(value = "/")
+    public ResponseEntity<?> delete(@RequestBody UserEntity userEntity) {
+        Long id = userEntity.getId();
 
+        boolean isExists = userService.existsById(id);
+
+        if(!isExists) {
+            return new ResponseEntity<>(Map.entry("message", NOT_FOUND_USER_BY_ID), HttpStatus.NOT_FOUND);
+        }
+
+        userService.delete(userEntity);
+
+        UserDto userDto = modelMapper.map(userEntity, UserDto.class);
+
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
 }
